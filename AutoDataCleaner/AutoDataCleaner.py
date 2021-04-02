b63fcd2ffa8cd2fcccf91f9c30d025586a2540f0
sinkingtitanic <elhoussinetalab@gmail.com>
2021-04-03 02:42:02 +0400

    major update

--- AutoDataCleaner/AutoDataCleaner.py
+++ AutoDataCleaner/AutoDataCleaner.py
@@ -1,59 +1,238 @@
 import pandas as pd
 
 na_cleaner_modes = ["remove row", "mean", "mode"]
-
-def clean_me(df, one_hot=True, na_cleaner_mode="mean", normalize=True, remove_columns=[], verbose=True):
+def clean_me(dataframe, 
+            detect_binary=True, 
+            numeric_dtype=True, 
+            one_hot=True, 
+            na_cleaner_mode="mean", 
+            normalize=True, 
+            datetime_columns=[], 
+            remove_columns=[], 
+            verbose=True):
     """
     clean_me function performs automatic dataset cleaning to Pandas DataFrame as per the settings parameters passed to this function
     
-    :param df: input Pandas DataFrame on which the cleaning will be performed 
+    :param dataframe: input Pandas DataFrame on which the cleaning will be performed 
+    :param detect_binray: if True, any column that has two unique values, these values will be replaced with 0 and 1 (e.g.: ['looser', 'winner'] => [0,1])
+    :param numeric_dtype: if True, columns will be converted to numeric dtypes when possible **see [1] in README.md**
     :param one_hot: if True, all non-numeric columns will be encoded to one-hot columns 
     :param na_cleaner_mode: what technique to use when dealing with None/NA/Empty values. Modes: 
-                            False       : do not consider cleaning na values 
-                            'remove row': removes rows with a cell that has NA value
-                            'mean'      : substitues empty NA cells with the mean of that column 
-                            'mode'      : substitues empty NA cells with the mode of that column
-                            '*'         : substiture empty NA cells with the value passed in 'na_cleaner_mode' param
+        False       : do not consider cleaning na values 
+        'remove row': removes rows with a cell that has NA value
+        'mean'      : substitues empty NA cells with the mean of that column 
+        'mode'      : substitues empty NA cells with the mode of that column
+        '*'         : substiture empty NA cells with the value passed in 'na_cleaner_mode' param
     :param normalize: if True, all non-binray (columns with values 0 or 1 are excluded) columns will be normalized. 
+    :param datetime_columns: a list of columns which contains date or time or datetime entries (important to be announced in this list, otherwise hot-encoding will mess up these columns)
     :param remove_columns: list of columns to remove, this is usually non-related featues such as the ID column 
     :param verbose: print progress in terminal/cmd
     :return: processed and clean Pandas DataFrame.
     """
+    df = dataframe.copy()
+
     if verbose:
-        print(" +++++++++++++++ DATA CLEANING STARTED ++++++++++++++++ ")
+        print(" +++++++++++++++ AUTO DATA CLEANING STARTED ++++++++++++++++ ")
     
     # Validating user input to __init__ function 
     assert type(one_hot) == type(True), "Please ensure that one_hot param is bool (True/False)"
     # assert na_cleaner_mode in na_cleaner_modes, "Please choose proper value for naCleaner parameter. (Correct values are only: {})".format(self.__naCleanerOps)
     assert type(df) == type(pd.DataFrame()), "Parameter 'df' should be Pandas DataFrame"
     
-    # converting non-numeric to one hot encoding 
-    if one_hot: 
-        if verbose: 
-            print(" = DataCleaner: Performing One-Hot encoding... ")
-        df = one_hot_df(df)
-    
-    # clean None (na) values 
-    if na_cleaner_mode != False: 
-        if verbose: 
-            print(" = DataCleaner: Performing None/NA/Empty values cleaning... ")
-        df = clean_na_df(df, na_cleaner_mode)
-    
-    # normalize all columns (binary 0,1 columns are excluded)
-    if normalize: 
-        if verbose: 
-            print(" = DataCleaner: Performing dataset normalization... ")
-        df = normalize_df(df)
 
-    # removing unwanted columns (usually its the ID columns...)
+
+    # casting datetime columns to datetime dtypes -----------------------------------------------------------------------
+    if len(datetime_columns) > 0:
+        if verbose: 
+            print(" =  AutoDataCleaner: Casting datetime columns to datetime dtype... ")
+        df = cols_to_datetime_dtype(df, datetime_columns, verbose)
+
+    # removing unwanted columns (usually its the ID columns...) ---------------------------------------------------------
     if len(remove_columns) > 0: 
         if verbose: 
-            print(" = DataCleaner: Performing removal of unwanted columns... ")
-        df = remove_columns_df(df, remove_columns)
+            print(" =  AutoDataCleaner: Performing removal of unwanted columns... ")
+        df = remove_columns_df(df, remove_columns, verbose)
 
+
+    # detecting binary columns ------------------------------------------------------------------------------------------
+    if detect_binary: 
+        if verbose: 
+            print(" =  AutoDataCleaner: Performing One-Hot encoding... ")
+        df = detect_binary_df(df, verbose)
+
+
+
+    # checking if any columns can be converted to numeric dtypes  -------------------------------------------------------
+    if numeric_dtype: 
+        if verbose: 
+            print(" = AutoDataCleaner: Converting columns to numeric dtypes when possible...")
+        df = convert_numeric_df(df, exclude=datetime_columns, force=False, verbose=verbose)
+
+
+
+    # converting non-numeric to one hot encoding ------------------------------------------------------------------------
+    if one_hot: 
+        if verbose: 
+            print(" =  AutoDataCleaner: Performing One-Hot encoding... ")
+        cols_num_before = df.shape[1]
+        df = one_hot_df(df)
+        if verbose: 
+            print("  + one-hot encoding done, added {} new columns".format(df.shape[1] - cols_num_before))
+    
+
+
+    # clean None (na) values --------------------------------------------------------------------------------------------
+    if na_cleaner_mode != False: 
+        if verbose: 
+            print(" =  AutoDataCleaner: Performing None/NA/Empty values cleaning... ")
+        df = clean_na_df(df, na_cleaner_mode, verbose)
+    
+
+
+    # normalize all columns (binary 0,1 columns are excluded) -----------------------------------------------------------
+    if normalize: 
+        if verbose: 
+            print(" =  AutoDataCleaner: Performing dataset normalization... ")
+        df = normalize_df(df, exclude=datetime_columns, verbose=verbose)
+
+    
     if verbose: 
-        print(" +++++++++++++++ DATA CLEANING FINISHED +++++++++++++++ ")
+        print(" +++++++++++++++ AUTO DATA CLEANING FINISHED +++++++++++++++ ")
     return df
+
+
+
+
+""" ------------------------------------------------------------------------------------------------------------------------- """
+
+
+import pandas as pd 
+
+def datetime_dtype_series(series, verbose=True):
+    """
+    datetime_dtype_series function casts date columns to datetime dtype 
+
+    :param df: input Pandas Series
+    :returns: processed Pandas Series
+    """
+    try: 
+        series = pd.to_datetime(series)
+        if verbose: 
+            print("  + converted column {} to datetime dtype".format(series.name))
+        return series
+    except Exception as e:
+        print(" ERROR {}".format(e))
+
+
+def cols_to_datetime_dtype(df, cols, verbose=True):
+    """
+    cols_to_datetime_dtype function casts given columns in dataframe to datetime dtype 
+
+    :param df: input Pandas DataFrame
+    :returns: processed Pandas DataFrame
+    """
+    for c in cols: 
+        df[c] = datetime_dtype_series(df[c], verbose)
+    return df
+
+
+def remove_columns_df(df, remove_columns, verbose=True): 
+    """
+    remove_columns_df function removes columns in 'remove_columns' param list and returns df 
+    
+    :param df: input Pandas DataFrame 
+    :param remove_columns: list of columns to be removed from the dataframe 
+    :param verbose: print progress in terminal/cmd
+    :returns: processed Pandas DataFrame 
+    """
+    stat = 0
+    for col in remove_columns: 
+        assert col in df.columns.to_list(), "{} is marked to be removed, but it does not exist in the dataset/dataframe".format(col)
+        
+        df.drop(columns=col, inplace=True)
+        stat += 1
+    if verbose: 
+        print("  + removed {} columns successfully.".format(stat))
+    return df
+
+def detect_binary_df(df, verbose=True): 
+    """
+    detect_binray function detects columns that has two unique values (e.g.: yes/no OR true/false etc...)
+    and converts it to a boolean column containing 0 or 1 values only 
+
+    :param df: input Pandas DataFrame 
+    :param verbose: print progress in terminal/cmd
+    :returns: processed Pandas DataFrame 
+    """
+    stat_cols = 0
+    stat_cols_names = [] 
+    stat_rows = 0
+    for col in df.columns.to_list(): 
+        # check if column has two unique values 
+        if len(df[col].unique().tolist()) == 2: 
+            unique_values = df[col].unique().tolist()
+            unique_values.sort() # to ensure consistency during training and predicting
+            df[col] = df[col].replace(unique_values[0], 0)
+            df[col] = df[col].replace(unique_values[1], 1)
+            stat_cols += 1
+            stat_cols_names.append(col)
+            stat_rows += df.shape[0]
+    if verbose: 
+        print("  + detected {} binary columns [{}], cells cleaned: {} cells".format(stat_cols, stat_cols_names, stat_rows))
+    return df
+
+def convert_numeric_series(series, force=False, verbose=True): 
+    """
+    convert_numeric_series function converts columns of dataframe to numeric dtypes when possible safely 
+    if the values that cannot be converted to numeric dtype are minority in the series (< %25), then
+    these minority values will be converted to NaN and the series will be forced to numeric dtype 
+
+    :param series: input Pandas Series
+    :param force: if True, values which cannot be casted to numeric dtype will be replaced with NaN 'see pandas.to_numeric() docs' (be careful with force=True)
+    :param verbose: print progress in terminal/cmd
+    :returns: Pandas series
+    """
+    stats = 0
+    if force: 
+        stats += series.shape[0]
+        return pd.to_numeric(series, errors='coerce'), stats
+    else: 
+        # checking if values that cannot be converted to numeric are < 25% of entries in this series
+        non_numeric_count = pd.to_numeric(series, errors='coerce').isna().sum()
+        if non_numeric_count/series.shape[0] < 0.25: 
+            # values that cannot be numeric are minority; hence, we set this as NaN and force that column to be
+            # casted to numeric dtype, the 'clean_na_series' function will handle these NaN values laters 
+            stats += series.shape[0]
+            if verbose and non_numeric_count != 0: 
+                print("  + {} minority (minority means < %25 of '{}' entries) values that cannot be converted to numeric dtype in column '{}' have been set to NaN, nan cleaner function will deal with them".format(non_numeric_count, series.name, series.name))
+            return pd.to_numeric(series, errors='coerce'), stats
+        else: 
+            # this series probably cannot be converted to numeric dtype, we will just leave it as is
+            return series, stats
+
+
+def convert_numeric_df(df, exclude=[], force=False, verbose=True):
+    """
+    convert_numeric_df function converts dataframe columns to numeric dtypes when possible safely 
+    if the values in a particular columns that cannot be converted to numeric dtype are minority in that column (< %25), then
+    these minority values will be converted to NaN and the column will be forced to numeric dtype 
+
+    :param df: input Pandas DataFrame
+    :param exclude: list of columns to be excluded whice converting dataframe columns to numeric dtype (usually datetime columns)
+    :param force: if True, values which cannot be casted to numeric dtype will be replaced with NaN 'see pandas.to_numeric() docs' (be careful with force=True)
+    :param verbose: print progress in terminal/cmd
+    :returns: Pandas DataFrame
+    """
+    stats = 0
+    for col in df.columns.to_list(): 
+        if col in exclude:
+            continue
+        df[col], stats_temp = convert_numeric_series(df[col], force, verbose)
+    stats += stats_temp
+    if verbose: 
+        print("  + converted {} cells to numeric dtypes".format(stats))
+    return df 
+
 
 def one_hot_df(df): 
     """ 
@@ -72,7 +251,7 @@ def clean_na_series(series, na_cleaner_mode):
     :param na_cleaner_mode: what cleaning technique to apply, 'na_cleaner_modes' for a list of all possibilities 
     :returns: cleaned version of the passed Series
     """
-    if na_cleaner_mode == "remove row": 
+    if na_cleaner_mode == 'remove row': 
         return series.dropna()
     elif na_cleaner_mode == 'mean':
         mean = series.mean()
@@ -80,49 +259,86 @@ def clean_na_series(series, na_cleaner_mode):
     elif na_cleaner_mode == 'mode':
         mode = series.mode()
         return series.fillna(mode)
+    elif na_cleaner_mode == False: 
+        return series
     else: 
         return series.fillna(na_cleaner_mode)
 
 
-def clean_na_df(df, na_cleaner_mode): 
+def clean_na_df(df, na_cleaner_mode, verbose=True): 
     """
     clean_na_df function cleans all columns in DataFrame as per given na_cleaner_mode
     
     :param df: input DataFrame
     :param na_cleaner_mode: what technique to apply to clean na values 
+    :param verbose: print progress in terminal/cmd
     :returns: cleaned Pandas DataFrame 
     """
+    stats = {}
     for col in df.columns.to_list(): 
+        if df[col].isna().sum() > 0 and verbose: 
+            stats[col + " NaN Values"] = df[col].isna().sum()
         df[col] = clean_na_series(df[col], na_cleaner_mode)
+    if verbose: 
+        print("  + cleaned the following NaN values: {}".format(stats))
     return df
 
-def normalize_df(df): 
+def normalize_df(df, exclude=[], verbose=True): 
     """
     normalize_df function performs normalization to all columns of dataframe excluding binary (1/0) columns 
     
     :param df: input Pandas DataFrame
+    :param exclude: list of columns to be excluded when performing normalization (usually datetime columns)
+    :param verbose: print progress in terminal/cmd
     :returns: normalized Pandas DataFrame 
     """
+    stats = 0
     for col in df.columns.to_list(): 
+        if col in exclude: 
+            continue
         # check if column is binray
         col_unique = df[col].unique().tolist()
         if len(col_unique) == 2 and 0 in col_unique and 1 in col_unique: 
             continue
         else: 
             df[col] = (df[col]-df[col].mean())/df[col].std()
+            stats += df.shape[0]
+    if verbose: 
+        print("  + normalized {} cells".format(stats))
     return df
 
-def remove_columns_df(df, remove_columns): 
+
+
+def help(): 
+    help_text = """
+    ++++++++++++++++++ AUTO DATA CLEANER HELP +++++++++++++++++++++++
+    FUNCTION CALL:
+    AutoDataCleaner.clean_me(df, one_hot=True, na_cleaner_mode="mean", normalize=True, remove_columns=[], verbose=True)
+
+    FUNCTION PARAMETERS:
+    df: input Pandas DataFrame on which the cleaning will be performed 
+    one_hot: if True, all non-numeric columns will be encoded to one-hot columns 
+    na_cleaner_mode: what technique to use when dealing with None/NA/Empty values. Modes: 
+        False: do not consider cleaning na values 
+        'remove row': removes rows with a cell that has NA value
+        'mean': substitues empty NA cells with the mean of that column  
+        'mode': substitues empty NA cells with the mode of that column
+        '*': any other value will substitute empty NA cells with that particular value passed here 
+    normalize: if True, all non-binray (columns with values 0 or 1 are excluded) columns will be normalized. 
+    remove_columns: list of columns to remove, this is usually non-related featues such as the ID column 
+    verbose: print progress in terminal/cmd
+    returns: processed and clean Pandas DataFrame 
+    ++++++++++++++++++ AUTO DATA CLEANER HELP +++++++++++++++++++++++
     """
-    remove_columns_df function removes columns in 'remove_columns' param list and returns df 
+    print(help_text)
+
+if __name__=="__main__": 
+    df = pd.DataFrame([
+                    [1, "Male", "white", 3, "2018/11/20"], 
+                    [2, "Female", "blue", "4", "2014/01/12"],
+                    [3, "Male", "white", 5, "2020/09/02"], 
+                    [4, "Male", "blue", "5", "2020/09/02"], 
+                    [5, "Male", "green", None, "2020/12/30"]
+                ], columns=['id', 'gender', 'color', 'weight', 'created_on'])
     
-    :param df: input Pandas DataFrame 
-    :param remove_columns: list of columns to be removed from the dataframe 
-    :returns: processed Pandas DataFrame 
-    """
-    for col in remove_columns: 
-        assert col in df.columns.to_list(), "{} is marked to be removed, but it does not exist in the dataset/dataframe".format(col)
-        
-        df.drop(columns=col, inplace=True)
-    return df
-
+    print(clean_me(df, remove_columns=['id'], datetime_columns=['created_on'])) # see 'Usage' section for more parameters
--- README.md
+++ README.md
@@ -1,17 +1,20 @@
 # AutoDataCleaner
 
-[![version](https://img.shields.io/badge/Version-1.0.3-lightgrey)](https://github.com/sinkingtitanic/AutoDataCleaner)
+[![version](https://img.shields.io/badge/Version-1.1.0-lightgrey)](https://github.com/sinkingtitanic/AutoDataCleaner)
 [![build](https://img.shields.io/badge/PypiBuild-Stable-blue)](https://pypi.org/project/AutoDataCleaner/)
 [![python-version](https://img.shields.io/badge/Python-3+-success)](https://www.python.org/downloads/)
 
-Simple and automatic data cleaning in one line of code! It performs **One Hot Encoding**, **Cleans Dirty/Empty Values**, **Normalizes values** and **Removes unwanted columns** all in one line of code.
+Simple and automatic data cleaning in one line of code! It performs  **one-hot encoding**, **converts columns to numeric dtype**, **cleaning dirty/empty values**, **normalizes values** and **removes unwanted columns** all in one line of code.
 Get your data ready for model training and fitting quickly.
 # Features 
 0. **Uses Pandas DataFrames [So, no need to learn new syntax]**
 1. **One-hot encoding**: encodes non-numeric values to one-hot encoding columns 
-2. **Normalization**: performs normalization to columns (excludes binary [1/0] columns)
-3. **Cleans Dirty/None/NA/Empty values**: replace None values with mean or mode of a column, delete row that has None cell or substitute None values with pre-defined value
-4. **Delete Unwanted Columns**: drop and remove unwanted columns (usually this will be the 'id' column)
+2. **Converts columns to numeric dtypes**: converts text numbers to numeric dtypes **see [1] below**
+3. **Auto detects binary columns**: any column that has two unique values, these values will be replaced with 0 and 1 (e.g.: `['looser', 'winner'] => [0,1]`)
+4. **Normalization**: performs normalization to columns (excludes binary [1/0] columns)
+5. **Cleans Dirty/None/NA/Empty values**: replace None values with mean or mode of a column, delete row that has None cell or substitute None values with pre-defined value
+6. **Delete Unwanted Columns**: drop and remove unwanted columns (usually this will be the 'id' column)
+7. **Converts date, time or datetime columns to datetime dtype**
 # Installation 
 #### Using pip
 `pip install AutoDataCleaner`
@@ -21,47 +24,67 @@ Clone repository and run `pip uninstall -e .` inside the repository directory
 Install from repository directly using `pip install git+git://github.com/sinkingtitanic/AutoDataCleaner.git#egg=AutoDataCleaner`
 # Quick One-line Usage: 
 ```
-    AutoDataCleaner.clean_me(df, 
-                            one_hot=True, 
-                            na_cleaner_mode="mean", 
-                            normalize=True, 
-                            remove_columns=[], 
-                            verbose=True)
+    import AutoDataCleaner.AutoDataCleaner as adc
+    adc.clean_me(dataframe, 
+            detect_binary=True, 
+            numeric_dtype=True, 
+            one_hot=True, 
+            na_cleaner_mode="mean", 
+            normalize=True, 
+            datetime_columns=[], 
+            remove_columns=[], 
+            verbose=True):
 ```
 # Example 
 ```
-import pandas as pd
-import AutoDataCleaner
+>>> import pandas as pd
+>>> import AutoDataCleaner.AutoDataCleaner as adc
+>>> 
+>>> df = pd.DataFrame([
+...     [1, "Male", "white", 3, "2018/11/20"], 
+...     [2, "Female", "blue", "4", "2014/01/12"],
+...     [3, "Male", "white", 5, "2020/09/02"], 
+...     [4, "Male", "blue", "5", "2020/09/02"], 
+...     [5, "Male", "green", None, "2020/12/30"]
+...     ], columns=['id', 'gender', 'color', 'weight', 'created_on'])
+>>> 
+>>> adc.clean_me(df, datetime_columns=['created_on'], remove_columns=['id']) # see 'Usage' section for more parameters
+ +++++++++++++++ AUTO DATA CLEANING STARTED ++++++++++++++++ 
+ =  AutoDataCleaner: Casting datetime columns to datetime dtype... 
+  + converted column created_on to datetime dtype
+ =  AutoDataCleaner: Performing removal of unwanted columns... 
+  + removed 1 columns successfully.
+ =  AutoDataCleaner: Performing One-Hot encoding... 
+  + detected 1 binary columns [['gender']], cells cleaned: 5 cells
+ = AutoDataCleaner: Converting columns to numeric dtypes when possible...
+  + 1 minority (minority means < %25 of 'weight' entries) values that cannot be converted to numeric dtype in column 'weight' have been set to NaN, nan cleaner function will deal with them
+  + converted 5 cells to numeric dtypes
+ =  AutoDataCleaner: Performing One-Hot encoding... 
+  + one-hot encoding done, added 2 new columns
+ =  AutoDataCleaner: Performing None/NA/Empty values cleaning... 
+  + cleaned the following NaN values: {'weight NaN Values': 1}
+ =  AutoDataCleaner: Performing dataset normalization... 
+  + normalized 5 cells
+ +++++++++++++++ AUTO DATA CLEANING FINISHED +++++++++++++++ 
+   gender    weight created_on  color_blue  color_green  color_white
+0       1 -1.507557 2018-11-20           0            0            1
+1       0 -0.301511 2014-01-12           1            0            0
+2       1  0.904534 2020-09-02           0            0            1
+3       1  0.904534 2020-09-02           1            0            0
+4       1  0.000000 2020-12-30           0            1            0
 
-df = pd.DataFrame([
-                    [1, "Green", 3], 
-                    [2, "Blue", 4],
-                    [3, "Green", 5], 
-                    [4, "Green", None]
-                ], columns=['id', 'color', 'weight'])
+```
 
-AutoDataCleaner.clean_me(df, remove_columns=['id']) # see 'Usage' section for more parameters
-```
-Example output:
-```
- +++++++++++++++ DATA CLEANING STARTED ++++++++++++++++ 
- = DataCleaner: Performing One-Hot encoding... 
- = DataCleaner: Performing None/NA/Empty values cleaning... 
- = DataCleaner: Performing dataset normalization... 
- = DataCleaner: Performing removal of unwanted columns... 
- +++++++++++++++ DATA CLEANING FINISHED +++++++++++++++ 
-	weight 	color_Blue 	color_Green
-0 	-0.855528 	0 	1
-1 	-0.475293 	1 	0
-2 	-0.095059 	0 	1
-3 	1.425880 	0 	0
-```
+**If you want to pick and choose with more customization, please go to `AutoDataCleaner.py` (the code is highly documented for your convenience)**
 # Explaining Parameters 
 
-`AutoDataCleaner.clean_me(df, one_hot=True, na_cleaner_mode="mean", normalize=True, remove_columns=[], verbose=True)`
+`adc.clean_me(dataframe, detect_binary=True, one_hot=True, na_cleaner_mode="mean", normalize=True, remove_columns=[], verbose=True)`
 
-Parameters & what do they mean:
-* `df`: input Pandas DataFrame on which the cleaning will be performed <br />
+Parameters & what do they mean
+_Call the help function `adc.help()` to output the below instructions_ 
+* `dataframe`: input Pandas DataFrame on which the cleaning will be performed <br />
+* `detect_binary`: if True, any column that has two unique values, these values will be replaced with 0 and 1 (e.g.: ['looser', 'winner'] => [0,1]) <br />
+* `numeric_dtype`: if True, columns will be converted to numeric dtypes when possible **see [1] below**
 * `one_hot`: if True, all non-numeric columns will be encoded to one-hot columns <br />
 * `na_cleaner_mode`: what technique to use when dealing with None/NA/Empty values. Modes: <br />
     * `False`: do not consider cleaning na values <br />
@@ -70,12 +93,18 @@ Parameters & what do they mean:
     * `'mode'`: substitues empty NA cells with the mode of that column<br />
     * `'*'`: any other value will substitute empty NA cells with that particular value passed here <br />
 * `normalize`: if True, all non-binray (columns with values 0 or 1 are excluded) columns will be normalized. <br />
+* `datetime_columns`: a list of columns which contains date or time or datetime entries (important to be announced in this list, otherwise `normalize_df` and `convert_numeric_df` functions  will mess up these columns)
 * `remove_columns`: list of columns to remove, this is usually non-related featues such as the ID column <br />
 * `verbose`: print progress in terminal/cmd<br />
 * `returns`: processed and clean Pandas DataFrame <br />
 
+[1] When `numeric_dtype` is set to True, columns that have strings of numbers (e.g.: "123" instead of 123) will be converted to numeric dtype. 
+if in a particular column, the values that cannot be converted to numeric dtypes are minority in that column (< 25% of total entries in that column), these
+minority non-numeric values in that column will be converted to NaN; then, the NaN cleaner function will handle them according to your settings. See `convert_numeric_df()` function in `AutoDataCleaner.py` file for more documentation.
+
 # Prediction 
-In prediction phase, put the examples to be predicted in Pandas DataFrame and run them through `AutoDataCleaner.clean_me` function **with the same parameters you
+In prediction phase, put the examples to be predicted in Pandas DataFrame and run them through `adc.clean_me()` function **with the same parameters you
 used during training**.
-# Contribution 
-Please feel free to send me feedback on "ofcourse7878@gmail.com", submit an issue or make a pull request! 
+
+# Contribution & Maintenance
+This repository is seriously commented for your convenience; please feel free to send me feedback on "ofcourse7878@gmail.com", submit an issue or make a pull request! 
