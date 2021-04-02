8dc1d9303ef74df43605166977c4a4b9ccaf2e96
sinkingtitanic <elhoussinetalab@gmail.com>
2021-04-02 17:48:27 +0400

    added pip installation

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-from AutoDataCleaner import __version__
+from AutoDataCleaner.AutoDataCleaner import __version__
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
     name='AutoDataCleaner',
@@ -9,6 +12,21 @@ setup(
     url='https://github.com/sinkingtitanic/AutoDataCleaner',
     author='Elhoussine Talab',
     author_email='ofcourse7878@gmail.com',
+    description="Python library to simply perform dataset cleaning on structured data stored in a Panda's DataFrame automatically with one line of code; to be used prior to training, e.g.: in data pre-processing phase in a machine learning project.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
 
-    py_modules=['AutoDataCleaner'],
+    packages=find_packages(),
+    install_requires=[
+    'pandas',
+    ],
+    classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Database"
+    ]
 )
