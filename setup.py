7bbe82f4caa7d7f274e21e576309c07c6094cbea
sinkingtitanic <elhoussinetalab@gmail.com>
2021-04-03 04:30:05 +0400

    updated description in setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     url='https://github.com/sinkingtitanic/AutoDataCleaner',
     author='Elhoussine Talab',
     author_email='ofcourse7878@gmail.com',
-    description="Simple and automatic data cleaning in one line of code! It performs One Hot Encoding, Cleans Dirty/Empty Values, Normalizes values and Removes unwanted columns all in one line of code. Get your data ready for model training and fitting quickly.",
+    description="Simple and automatic data cleaning in one line of code! It performs one-hot encoding, date & time casting to datetime dtype, detects binary columns, safely convert non-numeric columns to numeric dtypes, cleaning dirty/empty values, normalizing values and removing unwanted columns all in one line of code. Get your data ready for model training and fitting quickly.",
     long_description=long_description,
     long_description_content_type="text/markdown",
 
